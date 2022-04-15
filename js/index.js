d1d61e37437b207552eddb7ac4b5b02f00858fb6
fioreColoca <54822723+fioreColoca@users.noreply.github.com>
2022-04-14 21:32:45 -0300

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 document.addEventListener('DOMContentLoaded', function() {
     var elems = document.querySelectorAll('.collapsible');
     var instances = M.Collapsible.init(elems, {accordion: false});
+    var elems = document.querySelectorAll('.sidenav');
+    var instances = M.Sidenav.init(elems, { inDuration: 800 });
  });
 
 
-  
-  
+ 
